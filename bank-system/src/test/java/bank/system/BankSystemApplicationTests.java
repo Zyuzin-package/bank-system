d12d8407d168c763ab025e94cbd13b696046a17a
Zyuzin-package <zyuzinm4@gmail.com>
2023-08-28 18:33:18 +0400

    init project

---
+++ bank-system/src/test/java/bank/system/BankSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package bank.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
