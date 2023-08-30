4f0823907392a1f5d3ee34503589a1c96368b8c5
Zyuzin-package <zyuzinm4@gmail.com>
2023-08-30 22:05:13 +0400

    native querry

--- bank-system/src/main/java/bank/system/rest/dao/api/repository/PaymentEventRepository.java
+++ bank-system/src/main/java/bank/system/rest/dao/api/repository/PaymentEventRepository.java
@@ -1,9 +1,18 @@
 package bank.system.rest.dao.api.repository;
 
-import bank.system.model.PaymentEvent;
+import bank.system.model.domain.PaymentEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PaymentEventRepository extends JpaRepository<PaymentEvent, UUID> {
+    @Modifying
+    @Query(value = "Select * from payment_event where credit_offer_id  =:creditOfferId", nativeQuery = true)
+    @Transactional
+    List<PaymentEvent> getPaymentEventByOfferId(@Param("creditOfferId")UUID creditOfferId);
 }
