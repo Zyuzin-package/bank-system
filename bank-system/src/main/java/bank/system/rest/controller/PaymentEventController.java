ccd4d057a8d78cead8098b1bd1c177d223e45f02
Zyuzin-package <zyuzinm4@gmail.com>
2023-08-31 21:53:18 +0400

    rework for new program logic

--- bank-system/src/main/java/bank/system/rest/controller/PaymentEventController.java
+++ bank-system/src/main/java/bank/system/rest/controller/PaymentEventController.java
@@ -22,59 +22,17 @@ public class PaymentEventController {
 
     private final PaymentEventServiceImpl paymentEventServiceImpl;
 
-    private final CreditOfferServiceImpl creditOfferServiceImpl;
 
-    public PaymentEventController(PaymentEventServiceImpl paymentEventServiceImpl, CreditOfferServiceImpl creditOfferServiceImpl) {
+    public PaymentEventController(PaymentEventServiceImpl paymentEventServiceImpl) {
         this.paymentEventServiceImpl = paymentEventServiceImpl;
-        this.creditOfferServiceImpl = creditOfferServiceImpl;
     }
-
-    private String creditOfferId = "";
 
     @GetMapping("/paymentEvents/{offerId}")
     public String getPaymentsEvents(Model model, @PathVariable String offerId) {
         List<PaymentEvent> paymentEvents = paymentEventServiceImpl.getPaymentEventByOfferId(offerId);
 
-        this.creditOfferId = offerId;
         model.addAttribute("paymentEvents", paymentEvents);
         return "paymentEvents";
-    }
-
-    @GetMapping("/paymentEvents/new")
-    public String getCreatePage(Model model) {
-        model.addAttribute("paymentEvent", new PaymentEvent());
-
-        return "updatePaymentEvent";
-    }
-
-    @GetMapping("/paymentEvents/edit/{id}")
-    public String getUpdatePage(@PathVariable String id, Model model) {
-
-        PaymentEvent paymentEvent = paymentEventServiceImpl.findById(UUID.fromString(id));
-        model.addAttribute("paymentEvent", Objects.requireNonNullElseGet(paymentEvent, Client::new));
-
-        return "updatePaymentEvent";
-    }
-
-    @GetMapping("/paymentEvents/remove/{id}")
-    public String remove(@PathVariable String id) {
-        paymentEventServiceImpl.removeById(UUID.fromString(id));
-        return "redirect:/creditsOffers";
-    }
-
-    @PostMapping("/paymentEvents/new")
-    public String merge(PaymentEvent paymentEvent) {
-        CreditOffer creditOffer = creditOfferServiceImpl.findById(UUID.fromString(creditOfferId));
-        paymentEvent.setCreditOffer(creditOffer);
-
-        if (paymentEvent.getId() == null) {
-            paymentEventServiceImpl.save(paymentEvent);
-        } else {
-            paymentEventServiceImpl.update(paymentEvent);
-        }
-
-        this.creditOfferId = null;
-        return "redirect:/creditsOffers";
     }
 
 }
